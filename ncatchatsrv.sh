2e8c5e860f0fa4b03a0e7c98ed865b96afe2cbf2
rambius <rambiusparkisanius@gmail.com>
2023-12-10 23:42:27 +0000

    saving the pid in a file

--- ncatchatsrv.sh
+++ ncatchatsrv.sh
@@ -1,4 +1,7 @@
 #!/bin/sh
+
 mydir=$(dirname $(readlink -f $0))
 . ${mydir}/ncatchatcmn.sh
-/usr/pkg/bin/ncat --chat -l ${port} 
+/usr/pkg/bin/ncat --chat -l ${port} &
+ncat_pid=$!
+echo $ncat_pid > /var/run/ncatchatd.pid
